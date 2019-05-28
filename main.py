d1efcfbef24cdfa3903b280b7ef63ec30a794722
Ahri02 <51103601+Ahri02@users.noreply.github.com>
2019-05-28 10:40:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#import 
+import sys
+import socket
+import machine
+import time
+import network
+sta_if = network.WLAN(network.STA_IF)
+if not sta_if.isconnected():
+  print('connecting to network...')
+  sta_if.active(True)
+  sta_if.connect('NX569H', 'qin947012391dm')
+  #sta_if.connect('nubia', 'yellowlmf1234')
+  while not sta_if.isconnected():
+    pass
+print('network config:', sta_if.ifconfig())
