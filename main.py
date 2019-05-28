0f976883c76c764d67723207374f708811be12e8
Ahri02 <51103601+Ahri02@users.noreply.github.com>
2019-05-28 11:34:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ sta_if = network.WLAN(network.STA_IF)
 if not sta_if.isconnected():
   print('connecting to network...')
   sta_if.active(True)
-  sta_if.connect('NX569H', 'qin947012391dm')
+  sta_if.connect('NX569H', 'qin9470123921dm')
   #sta_if.connect('nubia', 'yellowlmf1234')
   while not sta_if.isconnected():
     pass
